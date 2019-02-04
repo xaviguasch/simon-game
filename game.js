332ff478a9ca4b9fefe5570bcbc9aba9a9988166
xaviguasch <xaviguasch@gmail.com>
2019-02-04 13:07:47 +0100

    listening to the selected btn and extracting its ID

--- game.js
+++ game.js
@@ -8,18 +8,21 @@ function nextSequence() {
 
     gamePattern.push(randomChosenColour)
 
+    // playing audio associated with randonChosenColour
     const audio = new Audio(`./sounds/${randomChosenColour}.mp3`)
-
     audio.play()
 
-
-    $(`#${randomChosenColour}`).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-
-    $(`#${randomChosenColour}`)
-
+    // Adding flickering effect to the randonChosenColour
+    $(`#${randomChosenColour}`).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100)
 
 }
 
 $(document).keypress(function () {
     nextSequence()
 });
+
+$('.btn').click(function () {
+    const userChosenColour = $(this).attr('id')
+    console.log(userChosenColour);
+
+})
