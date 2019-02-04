a0fe787612e865a1fe053751c8c0c1f2ef5105fc
xaviguasch <xaviguasch@gmail.com>
2019-02-04 12:47:16 +0100

    adding random selection of number/color and associating sound to it

---
+++ game.js
@@ -0,0 +1,23 @@
+const buttonColors = ['red', 'blue', 'green', 'yellow']
+const gamePattern = []
+
+
+function nextSequence() {
+    const randomNumber = Math.floor(Math.random() * 4)
+    const randomChosenColour = buttonColors[randomNumber]
+
+    gamePattern.push(randomChosenColour)
+
+    const audio = new Audio(`./sounds/${randomChosenColour}.mp3`)
+
+    audio.play()
+
+
+    $(`#${randomChosenColour}`).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    $(`#${randomChosenColour}`)
+
+
+}
+
+nextSequence()
