83a188a203c71cede5801931247f587dfdc757a3
xaviguasch <xaviguasch@gmail.com>
2019-02-04 13:22:20 +0100

    adding sound to the selecting btn and refactoring playSound()

--- game.js
+++ game.js
@@ -2,25 +2,11 @@ const buttonColors = ['red', 'blue', 'green', 'yellow']
 const gamePattern = []
 const userClickedPattern = []
 
-
-function nextSequence() {
-    const randomNumber = Math.floor(Math.random() * 4)
-    const randomChosenColour = buttonColors[randomNumber]
-
-    gamePattern.push(randomChosenColour)
-
-    // playing audio associated with randonChosenColour
-    const audio = new Audio(`./sounds/${randomChosenColour}.mp3`)
-    audio.play()
-
-    // Adding flickering effect to the randonChosenColour
-    $(`#${randomChosenColour}`).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100)
-
-}
-
 $(document).keypress(function () {
     nextSequence()
 });
+
+
 
 // detecting when a button is pressed
 $('.btn').click(function () {
@@ -30,4 +16,29 @@ $('.btn').click(function () {
     // adding the selected button color to the userClickedPattern array
     userClickedPattern.push(userChosenColour)
     console.log(userClickedPattern);
+
+    // playing audio associated with userChosenColour
+    playSound(userChosenColour)
 })
+
+
+
+function nextSequence() {
+    const randomNumber = Math.floor(Math.random() * 4)
+    const randomChosenColour = buttonColors[randomNumber]
+
+    gamePattern.push(randomChosenColour)
+
+    // playing audio associated with randonChosenColour
+    playSound(randomChosenColour)
+
+    // Adding flickering effect to the randonChosenColour
+    $(`#${randomChosenColour}`).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100)
+
+}
+
+
+function playSound(name) {
+    const audio = new Audio(`./sounds/${name}.mp3`)
+    audio.play()
+}
