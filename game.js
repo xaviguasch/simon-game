e1f20625b27ae4ed706ffadad31ebae11845a6c0
xaviguasch <xaviguasch@gmail.com>
2019-02-04 17:54:10 +0100

    adding startOver function

--- game.js
+++ game.js
@@ -48,6 +48,7 @@ function checkAnswer(currentLevel) {
             $('body').removeClass('game-over')
         }, 200);
         $('#level-title').text('Game Over, Press Any Key to Restart')
+        startOver()
     }
 }
 
@@ -80,4 +81,10 @@ function animatePress(currentColour) {
     setTimeout(() => {
         $(`#${currentColour}`).removeClass('pressed')
     }, 100);
+}
+
+function startOver() {
+    level = 0
+    gamePattern = []
+    started = false
 }
